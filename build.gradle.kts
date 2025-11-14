9a2987df0d8be176348d8cba5b12aacbf241b72b
Aleksandr Ivanov <johnmoviesite@gmail.com>
2025-11-14 17:10:13 +0100

    Add REST client

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,17 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+
 repositories {
     mavenCentral()
 }
 
+val ktor_version: String by project
+
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-client-core:${ktor_version}")
+    implementation("io.ktor:ktor-client-cio:${ktor_version}")
 }
 
 tasks.test {
